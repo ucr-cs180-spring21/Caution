a4984ee06c673522d79080a1e030d9fb106bf999
Danial <danny00b@gmail.com>
2021-04-08 01:36:37 -0700

    Cleaning up code

--- src/server.js
+++ src/server.js
@@ -1,24 +1,22 @@
+// Imports
 var express = require('express');  
 var app = express();  
 var server = require('http').createServer(app); 
 var io = require('socket.io')(server); 
 
-//keep track of how times clients have clicked the button
-var clickCount = 0;
-
-//redirect / to our index.html file
+// Index.html Redirect
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(client) { 
-	//when the server receives clicked message, do this
+	// Clicked messages
     client.on('hello', function(data) {
             console.log("Request received from client");
 		    io.emit('hello world');
     });
 });
 
-//start our web server and socket.io server listening
+// Starting the server and listening to the port
 server.listen(3000, function(){
 }); 
