e9b46e0b4ae87f7f064945ac4d107d68614196fc
Kalva014 <kalva014@ucr.edu>
2021-05-13 23:07:56 -0700

    Readded missing analytics

--- src/server.js
+++ src/server.js
@@ -169,25 +169,88 @@ io.on('connection', function(client) {
     });
     // Returns the frequency of each data field in a filter
     
-    // client.on('frequency_of_filter', function(id){
-    //     var re = []
-    //     let indexOfPassedInFilter = 0;
+    client.on('frequency_of_filter', function(id){
+        var re = []
+        let indexOfPassedInFilter = 0;
 
-    //     // Finds the specific filter in the data to get its contents
-    //     for(var i = 0; i < data.length; ++i) {
-    //         if(id === data[0][i]) {
-    //             break;
-    //         }
-    //         else {
-    //             indexOfPassedInFilter += 1;
-    //         }
-    //     }
+        // Finds the specific filter in the data to get its contents
+        for(var i = 0; i < data.length; ++i) {
+            if(id === data[0][i]) {
+                break;
+            }
+            else {
+                indexOfPassedInFilter += 1;
+            }
+        }
 
-    //     // Push all of the filter's fields to an array(HARDCODED RN JUST FOR WEATHER_CONDITION)
-    //     for(var j = 0; j < data.length; j++) {
-    //         re.push(data[j][indexOfPassedInFilter]);
-    //     }
-    //     console.log('Re ' + indexOfPassedInFilter);
+        // Push all of the filter's fields to an array(HARDCODED RN JUST FOR WEATHER_CONDITION)
+        for(var j = 0; j < data.length; j++) {
+            re.push(data[j][indexOfPassedInFilter]);
+        }
+        console.log('Re ' + indexOfPassedInFilter);
+    });
+
+        // Returns the analytics between two filters by getting the frequency of each data field between the two a filter
+        client.on('average_severity_of_filter', function(id, id2){
+        var re = []
+        var re2 = []
+        let indexOfPassedInFilter = 0;
+        let indexOfPassedInFilter2 = 0;
+
+        // Finds the specific filter in the data to get its contents
+        for(var i = 0; i < data.length; ++i) {
+            if(id === data[0][i]) {
+                break;
+            }
+            else {
+                indexOfPassedInFilter += 1;
+            }
+        }
+        
+        for(var i = 0; i < data.length; ++i) {
+            if(id2 === data[0][i]) {
+                break;
+            }
+            else {
+                indexOfPassedInFilter2 += 1;
+            }
+        }
+
+        // Push all of the filter's fields to an array
+        for(var j = 0; j < data.length; j++) {
+            re.push(data[j][indexOfPassedInFilter]); // Humidity
+            re2.push(data[j][indexOfPassedInFilter2]); // Severity
+        }
+        
+        // Holds each unique value of humidity
+        var analyticsArr = re.filter((value, index, self) => {return self.indexOf(value) === index;});
+
+        // Will hold the average severity of each humidity percent
+        var avgSeverity = Array(analyticsArr.length).fill(0);
+
+        // Counts the amount of times severity 
+        var counter = Array(analyticsArr.length).fill(0);
+
+        // Now get the average severity(id2) of each humidity percent(id)  
+        for(let i = 1; i < data.length; i++) {
+            for(let j = 1; j < analyticsArr.length; j++) {
+                if(re[i] == analyticsArr[j]) {
+                    counter[j] += 1; 
+                    avgSeverity[j] = avgSeverity[j] + parseFloat(re2[j]);
+                }
+            }
+        }
+        
+        // Have to make a separate loop to calculate correct average cause didn't when did embedded for loop
+        for(let j = 1; j < avgSeverity.length; j++) {
+                avgSeverity[j] = avgSeverity[j] / counter[j];
+            
+        }
+
+        re = [analyticsArr, avgSeverity]
+        console.log(re)
+        io.emit('filterFrequency', re);
+    });
 
     // client.on('csvexport', function(client){
     //     let csvContent = "data:text/csv;charset=utf-8,";
