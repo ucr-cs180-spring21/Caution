20b99ccd85f0820f614af4490b7b0ba624660823
Albert Dang <albert.dang46@gmail.com>
2021-05-26 18:57:19 -0700

    Simplified analytics and queries more

--- src/utils/Analytics.js
+++ src/utils/Analytics.js
@@ -1,31 +1,113 @@
+let frequencyGraphs = [
+    {
+        name: "Weather_Condition",
+        columnIndex: 31,
+        title: "Weather VS Frequency of Accidents",
+        titleX: "Weather",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "Humidity(%)",
+        columnIndex: 25,
+        title: "Humidity(%) VS Frequency of Accidents",
+        titleX: "Humidity(%)",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "Distance(mi)",
+        columnIndex: 10,
+        title: "Distance(mi) VS Frequency of Accidents",
+        titleX: "Distance(mi)",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "Bump",
+        columnIndex: 33,
+        title: "Bump VS Frequency of Accidents",
+        titleX: "Bump",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "Stop",
+        columnIndex: 41,
+        title: "Stop VS Frequency of Accidents",
+        titleX: "Stop",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "State",
+        columnIndex: 17,
+        title: "State VS Frequency of Accidents",
+        titleX: "State",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "Zipcode",
+        columnIndex: 18,
+        title: "Zipcode VS Frequency of Accidents",
+        titleX: "Zipcode",
+        titleY: "Frequency of Accidents"
+    },
+    {
+        name: "Side",
+        columnIndex: 14,
+        title: "Side VS Frequency of Accidents",
+        titleX: "Side",
+        titleY: "Frequency of Accidents"
+    }
+];
+
+let avgGraphs = [
+    {
+        name: "Weather_Condition,Severity",
+        xColumnIndex: 31,
+        yColumnIndex: 3,
+        title: "Weather VS Average Severity of Accidents",
+        titleX: "Weather",
+        titleY: "Average Severity of Accidents"
+    },
+    {
+        name: "Airport_Code,Severity",
+        xColumnIndex: 21,
+        yColumnIndex: 3,
+        title: "Airport VS Average Severity of Accidents",
+        titleX: "Airport",
+        titleY: "Average Severity of Accidents"
+    },
+    {
+        name: "Humidity(%),Severity",
+        xColumnIndex: 25,
+        yColumnIndex: 3,
+        title: "Humidity(%) VS Average Severity of Accidents",
+        titleX: "Humidity(%)",
+        titleY: "Average Severity of Accidents"
+    },
+    {
+        name: "Wind_Direction,Wind_Speed(mph)",
+        xColumnIndex: 28,
+        yColumnIndex: 29,
+        title: "Wind Direction VS Average Wind Speed(mph) of Accidents",
+        titleX: "Wind Direction",
+        titleY: "Average Wind Speed(mph) of Accidents"
+    },
+    {
+        name: "Weather_Condition,Visibility(mi)",
+        xColumnIndex: 31,
+        yColumnIndex: 27,
+        title: "Weather VS Average Visibility(mi) of Accidents",
+        titleX: "Weather",
+        titleY: "Average Visibility(mi) of Accidents"
+    }
+];
+
 function getGraphData(data, query) {
     let graphData;
 
-    switch (query) {
-        case 'Weather_Condition':
-            graphData = getFrequencyData(data, 31, "Weather VS Frequency of Accidents", "Weather", "Frequency of Accidents");
+    for (graph of frequencyGraphs) {
+        if (graph.name == query) {
+            graphData = getFrequencyData(data, graph.columnIndex, graph.title, graph.titleX, graph.titleY);
             break;
-        case 'Humidity(%)':
-            graphData = getFrequencyData(data, 25, "Humidity(%) VS Frequency of Accidents", "Humidity(%)", "Frequency of Accidents");
-            break;
-        case 'Distance(mi)':
-            graphData = getFrequencyData(data, 10, "Distance(mi) VS Frequency of Accidents", "Distance(mi)", "Frequency of Accidents");
-            break;
-        case 'Bump':
-            graphData = getFrequencyData(data, 33, "Bump VS Frequency of Accidents", "Bump", "Frequency of Accidents");
-            break;
-        case 'Stop':
-            graphData = getFrequencyData(data, 41, "Stop VS Frequency of Accidents", "Stop", "Frequency of Accidents");
-            break;
-        case 'State':
-            graphData = getFrequencyData(data, 17, "State VS Frequency of Accidents", "State", "Frequency of Accidents");
-            break;
-        case 'Zipcode':
-            graphData = getFrequencyData(data, 18, "Zipcode VS Frequency of Accidents", "Zipcode", "Frequency of Accidents");
-            break;
-        case 'Side':
-            graphData = getFrequencyData(data, 14, "Side VS Frequency of Accidents", "Side", "Frequency of Accidents");
-            break;
+        }
     }
     
     return graphData;
@@ -34,22 +116,11 @@ function getGraphData(data, query) {
 function getAvgGraphData(data, query) {
     let avgGraphData;
 
-    switch (query) {
-        case 'Weather_Condition,Severity':
-            avgGraphData = getAvgData(data, 31, 3, "Weather VS Average Severity of Accidents", "Weather", "Average Severity of Accidents");
+    for (graph of avgGraphs) {
+        if (graph.name == query) {
+            avgGraphData = getAvgData(data, graph.xColumnIndex, graph.yColumnIndex, graph.title, graph.titleX, graph.titleY);
             break;
-        case 'Airport_Code,Severity':
-            avgGraphData = getAvgData(data, 21, 3, "Airport VS Average Severity of Accidents", "Airport", "Average Severity of Accidents");
-            break;
-        case 'Humidity(%),Severity':
-            avgGraphData = getAvgData(data, 25, 3, "Humidity(%) VS Average Severity of Accidents", "Humidity(%)", "Average Severity of Accidents");
-            break;
-        case 'Wind_Direction,Wind_Speed(mph)':
-            avgGraphData = getAvgData(data, 28, 29, "Wind Direction VS Average Wind Speed(mph) of Accidents", "Wind Direction", "Average Wind Speed(mph) of Accidents");
-            break;
-        case 'Weather_Condition,Visibility(mi)':
-            avgGraphData = getAvgData(data, 31, 27, "Weather VS Average Visibility(mi) of Accidents", "Weather", "Average Visibility(mi) of Accidents");
-            break;
+        }
     }
 
     return avgGraphData;
--- src/utils/Query.js
+++ src/utils/Query.js
@@ -1,34 +1,58 @@
+let queries = [
+    {
+        name: "traffic_signal_true",
+        columnIndex: 43,
+        value: "TRUE"
+    },
+    {
+        name: "weather_condition_light_snow",
+        columnIndex: 31,
+        value: "Light Snow"
+    },
+    {
+        name: "humidity_100%",
+        columnIndex: 25,
+        value: "100"
+    },
+    {
+        name: "severity_3/3",
+        columnIndex: 3,
+        value: "3"
+    },
+    {
+        name: "timezone_us/eastern",
+        columnIndex: 20,
+        value: "US/Eastern"
+    },
+    {
+        name: "city_dayton",
+        columnIndex: 15,
+        value: "Dayton"
+    },
+    {
+        name: "airport_kday",
+        columnIndex: 21,
+        value: "KDAY"
+    },
+    {
+        name: "pressure_29.61_in",
+        columnIndex: 26,
+        value: "29.61"
+    }
+];
+
 function queryRecords(data, queryType) {
     let returnData;
 
-    switch (queryType) {
-        case "all_records":
-            returnData = data;
-            break;
-        case "traffic_signal_true":
-            returnData = getQueryResults(data, 43, 'TRUE');
-            break;
-        case "weather_condition_light_snow":
-            returnData = getQueryResults(data, 31, 'Light Snow');
-            break;
-        case "humidity_100%":
-            returnData = getQueryResults(data, 25, '100');
-            break;
-        case "severity_3/3":
-            returnData = getQueryResults(data, 3, '3');
-            break;
-        case "timezone_us/eastern":
-            returnData = getQueryResults(data, 20, 'US/Eastern');
-            break;
-        case "city_dayton":
-            returnData = getQueryResults(data, 15, 'Dayton');
-            break;
-        case "airport_kday":
-            returnData = getQueryResults(data, 21, 'KDAY');
-            break;
-        case "pressure_29.61_in":
-            returnData = getQueryResults(data, 26, '29.61');
-            break;
+    if (queryType == "all_records") {
+        returnData = data;
+    } else {
+        for (q of queries) {
+            if (q.name == queryType) {
+                returnData = getQueryResults(data, q.columnIndex, q.value);
+                break;
+            }
+        }
     }
 
     return returnData;
